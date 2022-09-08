841c8d04b11fb1eb722725931f33f7696e132934
miraj <miraj00@gmail.com>
2022-09-07 20:52:48 -0400

    CRS Policy error fixed

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -14,8 +14,8 @@ export class ApiService {
     constructor(private httpClient:HttpClient) { }
 
     getItems() : Observable<Blog>{
-      var returnVar = this.httpClient.get<Blog>(environment.apiEndpoint, 
-                          { headers: {'apiKey' : 'value'}, params: {'a' : 'b'} });
+      var returnVar = this.httpClient.get<Blog>(environment.apiEndpoint) 
+                          
 
       return returnVar;
     }
